4419df507430eac52ddb6e1cf9efd9d01d8a7e7d
Joenell Canlas Alonzo <medusazxc123@gmail.com>
2024-05-08 23:57:10 +0800

    Router bug fixes

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\ArticleController;
 use App\Http\Controllers\API\CommentVoteController;
 
 
@@ -14,15 +15,15 @@ Route::group(['prefix' => '/auth'], function () {
 });
 
 Route::group(['prefix' => '/article'], function () {
-    Route::get('/', [AuthController::class, 'index']);
-    Route::post('/create', [AuthController::class, 'store']);
-    Route::get('/{id}', [AuthController::class, 'show']);
-    Route::patch('/{id}', [AuthController::class, 'update']);
-    Route::delete('/{id}', [AuthController::class, 'destroy']);
+    Route::get('/', [ArticleController::class, 'index']);
+    Route::post('/create', [ArticleController::class, 'store']);
+    Route::get('/{id}', [ArticleController::class, 'show']);
+    Route::patch('/{id}', [ArticleController::class, 'update']);
+    Route::delete('/{id}', [ArticleController::class, 'destroy']);
 });
 
 Route::group(['prefix', '/cast'], function () {
     Route::get('/{id}', [CommentVoteController::class, 'getComments']);
-    Route::post('/comment' . [CommentVoteController::class, 'comment']);
+    Route::post('/comment', [CommentVoteController::class, 'comment']);
     Route::post('/vote', [CommentVoteController::class, 'vote']);
 });
