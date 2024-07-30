f3d86e85e1e558a832d6a6e010ccbc1e1664fe25
Joenell Alonzo <72643848+alonzojoe@users.noreply.github.com>
2024-07-30 09:09:45 +0800

    refactor: Article -> Comment relationship declare rules order by id on comments

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -31,7 +31,7 @@ class Article extends Model
 
     public function comments()
     {
-        return $this->hasMany(Comment::class);
+        return $this->hasMany(Comment::class)->orderBy('id', 'desc');
     }
 
     public function votes()
